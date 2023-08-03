471a91096ea42e287d33e30e1dace084bcea2bdd
DoumanAsh <douman@gmx.se>
2023-08-03 09:03:22 +0900

    Remove mention of obsolete feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,4 @@
 //! Primitive abstractions over OS identifiers.
-//!
-//! Features
-//!
-//! - `thread-name` Enables function `get_current_thread_name`
 
 #![no_std]
 #![cfg_attr(feature = "cargo-clippy", allow(clippy::style))]
